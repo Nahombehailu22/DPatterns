9e84bb48c46bf950a62fe2db1c432ac4a9f7c873
Nathan Kebede <nathanzelalem100@gmail.com>
2023-05-15 03:47:28 +0300

    Removed Commented out code from clientCodePython of AbstractFactory

--- src/ConvertToCode/PatternsClientCode/AbstractFactoryMethod/clientCodePython.js
+++ src/ConvertToCode/PatternsClientCode/AbstractFactoryMethod/clientCodePython.js
@@ -35,20 +35,4 @@ export const ClientCodePython = (nodes, setNodes) => {
     }
 
     return clientCode
-
-
-    // for (let key of nodeMap.keys()) {
-    //     if (key != 0){
-    //         clientCode += `\n\tcreator${key}` + " = " + nodeMap.get(key).class_name + "()";
-    //     }    
-    // }
-    // clientCode += "\n\t"
-    // for (let key of nodeMap.keys()) {
-    //     if (key != 0){
-    //         clientCode += `\n\t${`creator${key}` + "." + nodeMap.get(0).methods.find(method => method.id === "1").name + "()"}`
-    //     }
-        
-    // }
-    
-    return clientCode
 }
